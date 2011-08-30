371450a8db125c4afa44af7c1465f8f0963abddc
Robert Schoen <sambatux@web.de>
2011-08-30 12:37:39 +0200

    Removed mkdir creations. var2rd.sh takes this over.

--- os-config.sh
+++ os-config.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-#Title        : os-config.sh
-#Author       : sambaTux <sambatux@web.de>
-#Start date   : 09.08.2011
-#Finish date  : 13.08.2011
-#OS tested    : Ubuntu10.04
-#OS supported : Ubuntu10.04 ...
-#BASH version : 4.1.5(1)-release
-#Requires     : 
-#Version      : 0.1
-#Task(s)      : configure OS to run on Compact Flash
+# Title        : os-config.sh
+# Author       : sambaTux <sambatux@web.de>
+# Start date   : 09.08.2011
+# OS tested    : Ubuntu10.04
+# BASH version : 4.1.5(1)-release
+# Requires     : cp, sed, update-grub, tune2fs, apt-get update, mkdir
+# Version      : 0.1
+# Task(s)      : configure OS to run on Compact Flash
 
 
-#define vars
+# Define vars
 grub_conf='/etc/default/grub'     
-rd='ramdisk_size=170000'          #~170MB. Size for /dev/ramX devices.
+rd='ramdisk_size=170000'          #~170MB. Size for /dev/ram? devices.
 con='console=ttyS0,19200'         #serial console settings needed for soekris net5501
 el='elevator=noop'                #best for flash memory
 ply='noplymouth'                  #turn of plymouth boot splash
 
-#set kernel parameter 
+# Set kernel parameter 
 if [[ -f "$grub_conf" ]]; then
    cp -p "$grub_conf" ${grub_conf}.bak
    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="'$rd' '$con' '$el' '$ply'"/' "$grub_conf" \
    && update-grub
 fi
 
-#turn off counter based fsck for root dev
+# Turn off counter based fsck for root dev
 rootdev=`rdev | cut -d ' ' -f 1`
 tune2fs -c -1 "$rootdev"
 
-#deactivate  "multiverse" and "universe" repos in order to save ~60MB in /var/lib/apt/lists/
+# Deactivate  "multiverse" and "universe" repos in order to save ~60MB in /var/lib/apt/lists/
 sources="/etc/apt/sources.list"
 if [[ -f "$sources" ]]; then
    cp -p "$sources" ${sources}.bak
@@ -38,20 +36,12 @@ if [[ -f "$sources" ]]; then
    && apt-get update
 fi
 
-#set swappiness to 0 (def. = 60). 0 means that the kernel should preferably not use the swapp partition.
+# Set swappiness to 0 (def. = 60). 0 means that the kernel should preferably not use the swapp partition.
 sysctl="/etc/sysctl.conf"
 if [[ -f "$sysctl" ]]; then
    echo "" >>"$sysctl"
-   echo "#set swappiness to 0, meaning that the swapp partition should preferably not be used by the kernel" >>"$sysctl"
+   echo "# Set swappiness to 0, meaning that the swapp partition should preferably not be used by the kernel" >>"$sysctl"
    echo "vm.swappiness=0" >>"$sysctl"
 fi
-
-#create mount points for the logfiles of "var2rd.sh" and "varbak.sh"
-#The scripts will mount a tmpfs to hold the logfiles while their exec. 
-mkdir -m 700 /media/var2rd
-mkdir -m 700 /media/varbak
-
-#create /var/err. See var2rd.sh (at the top) to know why.  
-mkdir -m 700 /var/err
 
 exit 0
