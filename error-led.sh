37662c6903ad1467f65d3cf68162f2e5f67ee0e2
Robert Schoen <sambatux@web.de>
2011-08-30 17:50:05 +0200

    Added license

--- error-led.sh
+++ error-led.sh
@@ -12,6 +12,16 @@
 # NOTE         : See http://www.kernel.org/pub/linux/docs/device-list/devices.txt 
 #                for info about minor and major device numbers.
 
+# LICENSE      : Copyright (C) 2011 Robert Schoen
+
+#                This program is free software: you can redistribute it and/or modify it under the terms 
+#                of the GNU General Public License as published by the Free Software Foundation, either 
+#                version 3 of the License, or (at your option) any later version.
+#                This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+#                without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#                See the GNU General Public License for more details. [http://www.gnu.org/licenses/]
+
+
 # Define vars
 dev="/dev/error_led"
 # Define logfile. Use the same as the calling script.
