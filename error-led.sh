70b27c93fe830b5f64c0a0409ad084412a1b7a68
Robert Schoen <sambatux@web.de>
2011-08-30 13:07:28 +0200

    Added routine for warning LED and turn off warning LED

--- error-led.sh
+++ error-led.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-#Title        : error-led.sh
-#Author       : sambaTux <sambatux@web.de>
-#Start date   : 18.08.2011
-#Finish date  : 18.08.2011
-#OS tested    : Ubuntu10.04
-#OS supported : Ubuntu10.04 ...
-#BASH version : 4.1.5(1)-release
-#Requires     : modprobe, grep, cut, cat, mknod, rm, sleep
-#Version      : 0.1
-#Task(s)      : create node for soekris net5501 error LED and activate it
+# Title        : error-led.sh
+# Author       : sambaTux <sambatux@web.de>
+# Start date   : 18.08.2011
+# OS tested    : Ubuntu10.04
+# BASH version : 4.1.5(1)-release
+# Requires     : modprobe, grep, cut, cat, mknod, rm, sleep
+# Version      : 0.1
+# Task(s)      : create node for soekris net5501 error LED and de-/activate it
 
-#create error_led node:
+error_type="$1"
+
+# Create error_led node:
 dev="/dev/error_led"
 
-#load required modules
+# Load required modules
 modprobe cs5535_gpio
 modprobe pc8736x_gpio
 
 major=$(cat /proc/devices | grep "cs5535_gpio$" | cut -d ' ' -f 1)
 minor=6
 
-#create nod
+# Create nod
 if [[ -e "$dev" ]]; then
    rm -f "$dev"
    mknod "$dev" c $major $minor
@@ -29,16 +29,16 @@ else
    mknod "$dev" c $major $minor
 fi
 
-#GPIO
+# GPIO
 gpiodev="/dev/gpio"
 
 major=$(cat /proc/devices | grep "pc8736x_gpio$" | cut -d ' ' -f 1)
 
-# Minor values for GPIO. Theses one should work:
+# Minor values for GPIO. These ones should work:
 # 4 5 10 11 16 17 18 19 20 21 22 23 and 254
 minor=254
 
-#create nod
+# Create nod
 if [[ -e "$gpiodev" ]]; then
    rm -f "$gpiodev"
    mknod "$gpiodev" c $major $minor
@@ -46,12 +46,25 @@ else
    mknod "$gpiodev" c $major $minor
 fi
 
-#activate error led 
-while true; do
-  echo 1 >"$dev"
-  sleep 0.5
-  echo 0 >"$dev"
-  sleep 0.5
-done
+# Activate error led 
+case "$error_type" in
 
+   # A script failed/crashed
+   "--fatal")  echo 1 >"$dev"    
+   ;;
+
+   # A script doesn't run in optimal modus. 
+   # I.e. when not enough free RAM available for "varbak.sh"
+   "--warning") while true; do
+                  echo 1 >"$dev"
+                  sleep 1
+                  echo 0 >"$dev"
+                  sleep 1
+                done
+   ;; 
+   
+   # Deactivate warning led. I.e when "varbak.sh" has enough free RAM again.
+   "--warn-off")  echo 0 >"$dev"
+   ;;                 
+esac
 
